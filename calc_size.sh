6ccc64579ac489484c4e906ddf5c4705d0ac43f6
Ryu Ishizuka <ryu.ishzk@gmail.com>
2024-03-12 15:07:45 +0900

    add newline to calc_size.sh

--- calc_size.sh
+++ calc_size.sh
@@ -9,19 +9,19 @@ users=/System/Volumes/Data/Users/
 # Calculate folders
 echo "size\t/*" >> $sizelogfile
 sudo du -sh /* 2>/dev/null | awk '/[0-9]G/' | sort -rh >> $sizelogfile
-echo "size\t/System/*" >> $sizelogfile
+echo "\nsize\t/System/*" >> $sizelogfile
 sudo du -sh /System/* 2>/dev/null | awk '/[0-9]G/' | sort -rh >> $sizelogfile
-echo "size\t/System/Volumes/*" >> $sizelogfile
+echo "\nsize\t/System/Volumes/*" >> $sizelogfile
 sudo du -sh /System/Volumes/* 2>/dev/null | awk '/[0-9]G/' | sort -rh >> $sizelogfile
-echo "size\t/System/Volumes/Data/*" >> $sizelogfile
+echo "\nsize\t/System/Volumes/Data/*" >> $sizelogfile
 sudo du -sh /System/Volumes/Data/*  2>/dev/null | awk '/[0-9]G/' | sort -rh >> $sizelogfile
-echo "size\t"$users"*" >> $sizelogfile
+echo "\nsize\t"$users"*" >> $sizelogfile
 sudo du -sh "$users"*  2>/dev/null | awk '/[0-9]G/' | sort -rh >> $sizelogfile
-echo "size\t"$users""$username"/*" >> $sizelogfile
+echo "\nsize\t"$users""$username"/*" >> $sizelogfile
 sudo du -sh "$users""$username"/*  2>/dev/null | awk '/[0-9]G/' | sort -rh >> $sizelogfile
-echo "size\t"$users""$username"/Library/*" >> $sizelogfile
+echo "\nsize\t"$users""$username"/Library/*" >> $sizelogfile
 sudo du -sh "$users""$username"/Library/*  2>/dev/null | awk '/[0-9]G/' | sort -rh >> $sizelogfile
-echo "size\t"$users""$username"/Library/CloudStorage/*" >> $sizelogfile
+echo "\nsize\t"$users""$username"/Library/CloudStorage/*" >> $sizelogfile
 sudo du -sh "$users""$username"/Library/CloudStorage/*  2>/dev/null | awk '/[0-9]G/' | sort -rh >> $sizelogfile
-echo "size\t"$users""$username"/Library/Containers/*" >> $sizelogfile
+echo "\nsize\t"$users""$username"/Library/Containers/*" >> $sizelogfile
 sudo du -sh "$users""$username"/Library/Containers/*  2>/dev/null | awk '/[0-9]G/' | sort -rh >> $sizelogfile 
